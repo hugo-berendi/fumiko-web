32466a2ad1e9631afabb22376ddc95b0c6fb3361
hugo-berendi <hugo.berendi@outlook.de>
2022-11-13 14:06:39 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito  } from '@next/font/google'
+
+const font = Nunito()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={font.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
